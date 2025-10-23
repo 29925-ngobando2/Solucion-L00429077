1a97e600da49bf1191d55bfbc450d549e5ab6f62
NAOMIOBANDO <ngobando2@espe.edu.ec>
2025-10-23 15:36:52 -0500

    Creacion Repositorio
    Se adjunta la solucion

---
+++ Parte_III/validar.js
@@ -0,0 +1,35 @@
+﻿function validar() {
+  var correo, telefono, expresion;
+  
+  correo = document.getElementById('txtEmail').value;
+  telefono = document.getElementById('txtTelefono').value;
+  expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+
+  // Validar correo
+  if (!expresion.test(correo)) {
+    alert("El correo no es válido");
+    return false;
+  }
+
+  // Validar telefono
+  if (telefono.length != 10) {
+    alert("El teléfono no es correcto");
+    return false;
+  } else if (isNaN(telefono)) {
+    alert("El teléfono ingresado no es un número");
+    return false;
+  }
+
+  // Si todo es correcto
+  alert("Datos ingresados correctamente");
+
+
+  //Cambiar el mensaje que muestra el botón. Del original "Enviar" a "Enviando datos..."
+  var boton = document.getElementById('btnEnviar');
+  //desabilitar boton
+  boton.disabled = true;
+  boton.value = "Enviando datos.......";
+
+  // Enviar el formulario con this.form.submit()
+  boton.form.submit();
+}
